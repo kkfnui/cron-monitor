dd136780ccb1e64e3502325145ba45f6403a2732
lvfei <lvfei@xunlei.com>
2017-12-23 01:41:54 +0800

    添加数据模型

--- server/web/task/models.py
+++ server/web/task/models.py
@@ -1,6 +1,35 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
 
 # Create your models here.
+
+
+class Task(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=128)
+    key = models.CharField(max_length=128)
+
+    create_by = models.CharField(max_length=64)
+    update_by = models.CharField(max_length=64)
+    created_at = models.DateTimeField()
+    update_at = models.DateTimeField()
+    invalid = models.BooleanField()
+
+    last_status = models.IntegerField()
+    last_cost = models.IntegerField()
+    last_run = models.DateTimeField()
+
+    cost_threadhold = models.IntegerField()
+    idle_interval_threadhold = models.IntegerField()
+
+
+class Job(models.Model):
+    id = models.AutoField(primary_key=True)
+    task_id = models.IntegerField()
+    task_name = models.CharField(max_length=128)
+    start_at = models.DateTimeField()
+    stop_at = models.DateTimeField()
+    status = models.IntegerField()
+    cost = models.IntegerField()
--- server/web/task/views.py
+++ server/web/task/views.py
@@ -2,5 +2,21 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from rest_framework import viewsets
+
+# Create your views here.
+
+
+class TaskViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
+
+    # def list(self, request):
+    #     queryset = Cat.objects.all().order_by('-created_at')
+    #     serializer = CatSerializer(queryset, many=True)
+    #     return Response(serializer.data)
 
 # Create your views here.
